82009f999589234bbbe882e8f3bf59731bc53ef5
xulantong <1457501818@qq.com>
2022-07-13 11:14:26 +0800

    dev-component

--- src/peanut/components/peanutDescriptions/components/AutoFitEditor/index.vue
+++ src/peanut/components/peanutDescriptions/components/AutoFitEditor/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="AutoFitEditor">
-        <template v-if="edit">
+        <div class="input-item" v-if="edit">
             <el-input v-if="type === 'string'" size="mini" v-model="modelValue"></el-input>
-        </template>
+        </div>
         <div v-else>{{ modelValue || "-" }}</div>
     </div>
 </template>
@@ -36,37 +36,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 .AutoFitEditor {
-    position: relative;
     line-height: 28px;
-
-    .el-cascader,
-    .el-select,
-    .el-date-editor,
-    .el-input-number,
-    .el-autocomplete,
-    .el-input {
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        width: unset !important;
-        line-height: 28px;
-
-        .el-select__input {
-            margin-left: 4px;
-        }
-
-        .el-input__inner {
-            padding: 0 10px !important;
-            height: 100%;
-            width: 100%;
-            font-size: 14px;
-            font-weight: 400;
-        }
-    }
-
-
 }
 
 </style>
